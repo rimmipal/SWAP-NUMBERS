afd0a5dd53511ce777f80646ab3fbf1eba58fb20
Rimmi Pal <rimmipal01@gmail.com>
2026-07-02 13:49:23 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter the first number :"))
+num2 = int(input("enter the second number :"))
+
+print("Before swapping")
+print("num1 =", num1)
+print("num2 =", num2)
+
+num1, num2 = num2, num1
+
+print("After swapping")
+print("num1 =", num1)
+print("num2 =", num2)
